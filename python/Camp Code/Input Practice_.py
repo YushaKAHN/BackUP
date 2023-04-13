48d19df82b283a5446da392423351a2b9bd9bc9d
YushaKAHN <125352716+YushaKAHN@users.noreply.github.com>
2023-04-13 11:41:44 +0200

    Add files via upload

---
+++ python/Camp Code/Input Practice_.py
@@ -0,0 +1,13 @@
+# input practice 
+enterName = input("What is your name?:")
+pAge = input("When were you born?:")
+
+PAge1 = int(2023 - int(pAge))
+#first interaction
+print("Hi",enterName,"i am the interface ready to take you through some functions")
+print("Please confirm your Age, are you currently:",PAge1)
+
+#confirm age 
+
+AgeCon = input("Yes/No")
+
---
+++ python/Camp Code/Working_list_chapter9.py
@@ -0,0 +1,10 @@
+#a = [1,2,3]
+#b =[ 4,5,6]
+#c =a+b
+#print(c)
+
+#list methods
+#x = list()
+# <list type = (x)>
+ #   <dir(x)>
+ #  ["append","count","extend","index","instert","pop,","remove","reverse","sort"]
